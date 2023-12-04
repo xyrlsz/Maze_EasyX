2e42c6088848f4e4d5f7e5691290cf48fa4eece7
xyrlsz <2434221948@qq.com>
2023-12-05 00:22:49 +0800

    huitui

--- solver/BFS.h
+++
@@ -1,6 +0,0 @@
-#ifndef BFS_H_
-#define BFS_H_
-#include <queue>
-#include <iostream>
-std::queue<std::pair<int, int>> BFS(std::vector<std::vector<uint32_t>> maze, int x1, int y1, int x2, int y2);
-#endif
--- solver/DFS.h
+++
@@ -1,9 +0,0 @@
-#ifndef DFS_H_
-#define DFS_H_
-#include <iostream>
-#include <vector>
-#include <queue>
-
-std::queue<std::pair<int, int>> DFS(std::vector<std::vector<uint32_t>> maze, int x1, int y1, int x2, int y2);
-#endif 
-
